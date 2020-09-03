c58a3afa350d5bf96cec891580fe09c4bb573ada
chyWg <wangcy1202@qq.com>
2020-09-03 19:20:56 +0800

    Update mac_install.sh

--- mac_install.sh
+++ mac_install.sh
@@ -6,7 +6,7 @@ else
   unzip /tmp/wget.zip -d /tmp/
   chmod +x /tmp/wget-master/wget
 fi
-wget -O /tmp/卡巴斯基for\ mac安装包.zip "https://megvii-my.sharepoint.cn/:u:/g/personal/wangchongyang_megvii_com/ESUaE18_b9lNhxre80DV_VoB8CwXrF2GBCCjY0nzJ-II8A?e=oJDtat?download=1"
+wget -O /tmp/卡巴斯基for\ mac安装包.zip "https://megvii-my.sharepoint.cn/:u:/g/personal/wangchongyang_megvii_com/ESUaE18_b9lNhxre80DV_VoB8CwXrF2GBCCjY0nzJ-II8A?download=1"
 cd /tmp/
 ditto -V -x -k --sequesterRsrc --rsrc /tmp/卡巴斯基for\ mac安装包.zip .
 cd 卡巴斯基for\ mac安装包/kesmac11.0.1.753
